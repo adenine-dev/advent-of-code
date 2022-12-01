1c7e3f94f824a45ccd64e492c6fa5f38ba21f6fe
Adenine <adenine.dev@gmail.com>
2022-12-01 14:00:18 -0500

    day 1 part 1

--- day_01_calorie_counting/src/main.rs
+++ day_01_calorie_counting/src/main.rs
@@ -1,3 +1,26 @@
+use std::error::Error;
+
+fn calorie_count(input: &str) -> Result<u32, &'static str> {
+    input
+        .split("\n\n")
+        .map(|x| (x.split('\n').flat_map(|s| s.parse::<u32>()).sum::<u32>()))
+        .max()
+        .ok_or("invalid input.")
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", calorie_count(include_str!("input.txt")).unwrap());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            calorie_count(include_str!("test.txt"),).expect("failed: "),
+            24000
+        );
+    }
 }
