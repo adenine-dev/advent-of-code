82b2917f996328d8d8a1d1498f26a444242929f2
Adenine <adenine.dev@gmail.com>
2022-12-02 15:02:36 -0500

    day 2 part 2

--- day_02_rock_paper_scissors/src/main.rs
+++ day_02_rock_paper_scissors/src/main.rs
@@ -1,4 +1,4 @@
-fn rock_paper_scissors(input: &str) -> u32 {
+fn rock_paper_scissors_rps(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
@@ -23,10 +23,42 @@ fn rock_paper_scissors(input: &str) -> u32 {
         .sum()
 }
 
+fn rock_paper_scissors_wld(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (other, me) = line.split_once(' ').unwrap();
+            let outcome = match me {
+                "X" => 0, // lose
+                "Y" => 1, // draw
+                "Z" => 2, // win
+                &_ => unreachable!(),
+            };
+
+            let other_code = match other {
+                "A" => 0, // rock
+                "B" => 1, // paper
+                "C" => 2, // scissors
+                &_ => unreachable!(),
+            };
+
+            let win_score = outcome * 3;
+            let shape_score = (other_code + outcome) % 3;
+            let shape_score = if shape_score == 0 { 3 } else { shape_score };
+            shape_score + win_score
+        })
+        .sum()
+}
+
 fn main() {
     println!(
-        "projected score: {}",
-        rock_paper_scissors(include_str!("input.txt"))
+        "projected score if opponents moves are encoded: {}",
+        rock_paper_scissors_rps(include_str!("input.txt"))
+    );
+
+    println!(
+        "projected score if outcome is encoded: {}",
+        rock_paper_scissors_wld(include_str!("input.txt"))
     );
 }
 
@@ -36,6 +68,7 @@ mod test {
 
     #[test]
     fn test() {
-        assert_eq!(rock_paper_scissors(include_str!("test.txt")), 15);
+        assert_eq!(rock_paper_scissors_rps(include_str!("test.txt")), 15);
+        assert_eq!(rock_paper_scissors_wld(include_str!("test.txt")), 12);
     }
 }
