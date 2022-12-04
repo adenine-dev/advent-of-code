7114caa0222677210df8f564fdf2c6741d807cd5
Adenine <adenine.dev@gmail.com>
2022-12-03 19:21:24 -0500

    day 3 part 2

--- day_03_rucksack_reorganization/src/main.rs
+++ day_03_rucksack_reorganization/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(int_log)]
+#![feature(iter_array_chunks)]
 
 fn ascii_to_priority(c: u8) -> u64 {
     let i = (c) ^ 0b1100000;
@@ -23,10 +24,33 @@ fn rucksack_reorganization(input: &str) -> u32 {
         .sum::<u32>()
 }
 
+fn badge_finder(input: &str) -> u32 {
+    input
+        .lines()
+        .array_chunks::<3>()
+        .map(|line| {
+            let masks = line.map(|s| {
+                let mut mask = 0u64;
+                s.as_bytes().iter().for_each(|c| {
+                    mask |= 1 << ascii_to_priority(*c);
+                });
+                mask
+            });
+
+            (masks[0] & masks[1] & masks[2]).ilog2()
+        })
+        .sum::<u32>()
+}
+
 fn main() {
     println!(
         "sum of priorities of mixed item is: {}",
         rucksack_reorganization(include_str!("input.txt"))
+    );
+
+    println!(
+        "sum of priorities of badges is: {}",
+        badge_finder(include_str!("input.txt"))
     );
 }
 
@@ -37,5 +61,6 @@ mod test {
     #[test]
     fn test() {
         assert_eq!(rucksack_reorganization(include_str!("test.txt")), 157);
+        assert_eq!(badge_finder(include_str!("test.txt")), 70);
     }
 }
