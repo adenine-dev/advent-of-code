f0f81db8c8abc5440ab5b42997a8412f35e9df24
Adenine <adenine.dev@gmail.com>
2022-12-04 22:38:03 -0500

    day 4 part 1

--- day_04_camp_cleanup/src/main.rs
+++ day_04_camp_cleanup/src/main.rs
@@ -1,3 +1,31 @@
+fn count_fully_contained(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let [a1, a2, b1, b2] = &line
+                .split([',', '-'])
+                .flat_map(|num| num.parse::<u32>())
+                .collect::<Vec<_>>()[..] 
+            else { panic!("invalid input") };
+
+            u32::from((a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2))
+        })
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "number of pairs where one fully contains the other: {}",
+        count_fully_contained(include_str!("input.txt"))
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(count_fully_contained(include_str!("test.txt")), 2);
+    }
 }
