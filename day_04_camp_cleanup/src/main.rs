6ec35081a1e6133c0cfcdd139b65d746513f67b6
Adenine <adenine.dev@gmail.com>
2022-12-04 22:44:57 -0500

    day 4 part 2

--- day_04_camp_cleanup/src/main.rs
+++ day_04_camp_cleanup/src/main.rs
@@ -13,10 +13,31 @@ fn count_fully_contained(input: &str) -> u32 {
         .sum()
 }
 
+fn count_overlapping(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let [a1, a2, b1, b2] = &line
+                .split([',', '-'])
+                .flat_map(|num| num.parse::<u32>())
+                .collect::<Vec<_>>()[..] 
+            else { panic!("invalid input") };
+
+            u32::from(a1 <= b2 && b1 <= a2)
+        })
+        .sum()
+}
+
+
 fn main() {
     println!(
         "number of pairs where one fully contains the other: {}",
         count_fully_contained(include_str!("input.txt"))
+    );
+    
+    println!(
+        "number of pairs that overlap: {}",
+        count_overlapping(include_str!("input.txt"))
     );
 }
 
@@ -27,5 +48,6 @@ mod test {
     #[test]
     fn test() {
         assert_eq!(count_fully_contained(include_str!("test.txt")), 2);
+        assert_eq!(count_overlapping(include_str!("test.txt")), 4);
     }
 }
