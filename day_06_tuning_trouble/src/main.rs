4be6ff0ea78f6c4cfe9d6d9dbf2180fb4e80c53a
Adenine <adenine.dev@gmail.com>
2022-12-06 22:33:59 -0500

    day 6 part 2

--- day_06_tuning_trouble/src/main.rs
+++ day_06_tuning_trouble/src/main.rs
@@ -1,14 +1,14 @@
-fn first_start_of_packet(input: &str) -> usize {
-    for (idx, slice) in input.as_bytes().windows(4).enumerate() {
-        if slice[0] != slice[1]
-            && slice[0] != slice[2]
-            && slice[0] != slice[3]
-            && slice[1] != slice[2]
-            && slice[1] != slice[3]
-            && slice[2] != slice[3]
-        {
-            return idx + 4;
+use std::collections::HashSet;
+
+fn first_start_of_marker(input: &str, unique_chars: usize) -> usize {
+    'outer: for (idx, slice) in input.as_bytes().windows(unique_chars).enumerate() {
+        let mut hs = HashSet::new();
+        for c in slice {
+            if !hs.insert(c) {
+                continue 'outer;
+            }
         }
+        return idx + unique_chars;
     }
     0
 }
@@ -16,7 +16,11 @@ fn first_start_of_packet(input: &str) -> usize {
 fn main() {
     println!(
         "first start of packet occurs at {}",
-        first_start_of_packet(include_str!("input.txt"))
+        first_start_of_marker(include_str!("input.txt"), 4)
+    );
+    println!(
+        "first start of message occurs at {}",
+        first_start_of_marker(include_str!("input.txt"), 14)
     );
 }
 
@@ -26,16 +30,40 @@ mod test {
 
     #[test]
     fn test() {
-        assert_eq!(first_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-        assert_eq!(first_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(first_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"), 6);
         assert_eq!(
-            first_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            first_start_of_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4),
+            7
+        );
+        assert_eq!(first_start_of_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(first_start_of_marker("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(
+            first_start_of_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4),
             10
         );
         assert_eq!(
-            first_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            first_start_of_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4),
             11
+        );
+
+        assert_eq!(
+            first_start_of_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14),
+            19
+        );
+        assert_eq!(
+            first_start_of_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14),
+            23
+        );
+        assert_eq!(
+            first_start_of_marker("nppdvjthqldpwncqszvftbrmjlhg", 14),
+            23
+        );
+        assert_eq!(
+            first_start_of_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
+            29
+        );
+        assert_eq!(
+            first_start_of_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14),
+            26
         );
     }
 }
