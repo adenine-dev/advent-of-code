5f70b298d9135feed25d3dba0a6a94a1498cca1c
Adenine <adenine.dev@gmail.com>
2022-12-06 22:24:18 -0500

    day 6 part 1

--- day_06_tuning_trouble/src/main.rs
+++ day_06_tuning_trouble/src/main.rs
@@ -1,3 +1,41 @@
+fn first_start_of_packet(input: &str) -> usize {
+    for (idx, slice) in input.as_bytes().windows(4).enumerate() {
+        if slice[0] != slice[1]
+            && slice[0] != slice[2]
+            && slice[0] != slice[3]
+            && slice[1] != slice[2]
+            && slice[1] != slice[3]
+            && slice[2] != slice[3]
+        {
+            return idx + 4;
+        }
+    }
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "first start of packet occurs at {}",
+        first_start_of_packet(include_str!("input.txt"))
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(first_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(first_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(first_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(
+            first_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            10
+        );
+        assert_eq!(
+            first_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            11
+        );
+    }
 }
