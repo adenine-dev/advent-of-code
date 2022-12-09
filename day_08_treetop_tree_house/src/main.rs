d6d4c49f16ec6a34dc0e75333b82d7275fd9eb5f
Adenine <adenine.dev@gmail.com>
2022-12-08 20:20:12 -0500

    day 8 init

---
+++ day_08_treetop_tree_house/src/main.rs
@@ -0,0 +1,17 @@
+fn day_fn(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    println!("input: {}", day_fn(include_str!("input.txt")));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(day_fn(include_str!("test.txt")), 21);
+    }
+}
