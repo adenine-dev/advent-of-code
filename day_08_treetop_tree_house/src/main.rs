1d2ca5e1a6cc827fe39db9bebdc05ccee27a737e
Adenine <adenine.dev@gmail.com>
2022-12-08 23:11:17 -0500

    day 8 part 2

--- day_08_treetop_tree_house/src/main.rs
+++ day_08_treetop_tree_house/src/main.rs
@@ -1,3 +1,38 @@
+// adapted from std::iterator::TakeWhile
+
+#[derive(Clone)]
+pub struct TakeWhileInclusive<I: Iterator, P: FnMut(&I::Item) -> bool> {
+    iter: I,
+    flag: bool,
+    predicate: P,
+}
+
+impl<I: Iterator, P: FnMut(&I::Item) -> bool> TakeWhileInclusive<I, P> {
+    pub fn new(iter: I, predicate: P) -> Self {
+        TakeWhileInclusive {
+            iter,
+            flag: false,
+            predicate,
+        }
+    }
+}
+
+impl<I: Iterator, P: FnMut(&I::Item) -> bool> Iterator for TakeWhileInclusive<I, P> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        if self.flag {
+            None
+        } else {
+            let next = self.iter.next()?;
+            if !(self.predicate)(&next) {
+                self.flag = true;
+            }
+            Some(next)
+        }
+    }
+}
+
 fn visible_trees(input: &str) -> u32 {
     let grid = input
         .lines()
@@ -8,7 +43,6 @@ fn visible_trees(input: &str) -> u32 {
         })
         .collect::<Vec<_>>();
 
-    let mut bool_grid = vec![vec![0; grid.len()]; grid.len()];
     let mut sum = 0;
     for (i, row) in grid.iter().enumerate() {
         for (j, &height) in row.iter().enumerate() {
@@ -23,20 +57,62 @@ fn visible_trees(input: &str) -> u32 {
                 || up_iter.max().unwrap_or(-1) < height
             {
                 sum += 1;
-                bool_grid[i][j] = 1;
             }
         }
     }
 
-    // dbg!(bool_grid);
-
     sum
+}
+
+fn best_tree_score(input: &str) -> u32 {
+    let grid = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as i32)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut scores =
+        vec![vec![(usize::MAX, usize::MAX, usize::MAX, usize::MAX); grid.len()]; grid.len()];
+
+    let x = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(j, &height)| {
+                    let down = grid.iter().map(|x| x[j]).skip(i + 1);
+                    let up = grid.iter().map(|x| x[j]).rev().skip(grid.len() - i);
+                    let right = row.iter().skip(j + 1);
+                    let left = row.iter().rev().skip(row.len() - j);
+
+                    scores[i][j] = (
+                        TakeWhileInclusive::new(down, |x| *x < height).count(),
+                        TakeWhileInclusive::new(up, |x| *x < height).count(),
+                        TakeWhileInclusive::new(right, |x| **x < height).count(),
+                        TakeWhileInclusive::new(left, |x| **x < height).count(),
+                    );
+                    scores[i][j].0 * scores[i][j].1 * scores[i][j].2 * scores[i][j].3
+                })
+                .max()
+        })
+        .max()
+        .unwrap() as u32;
+    x
 }
 
 fn main() {
     println!(
         "number of visible trees: {}",
         visible_trees(include_str!("input.txt"))
+    );
+
+    println!(
+        "best tree score: {}",
+        best_tree_score(include_str!("input.txt"))
     );
 }
 
@@ -47,5 +123,6 @@ mod test {
     #[test]
     fn test() {
         assert_eq!(visible_trees(include_str!("test.txt")), 21);
+        assert_eq!(best_tree_score(include_str!("test.txt")), 8);
     }
 }
