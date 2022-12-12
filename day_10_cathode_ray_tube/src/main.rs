cee81907908ceeabfcf75c6c78401f44123260d3
Adenine <adenine.dev@gmail.com>
2022-12-12 00:31:21 -0500

    day 10 part 1

--- day_10_cathode_ray_tube/src/main.rs
+++ day_10_cathode_ray_tube/src/main.rs
@@ -1,9 +1,49 @@
-fn day_fn(input: &str) -> u32 {
-    0
+fn signal_strength(input: &str, cycles: &[u32]) -> i32 {
+    let mut cycle = 1;
+    let mut x = 1;
+    input
+        .lines()
+        .flat_map(|line| {
+            let (command, arg) = line.split_at(4);
+            match command {
+                "noop" => {
+                    cycle += 1;
+                    [
+                        if cycles.contains(&(cycle - 1)) {
+                            Some(x)
+                        } else {
+                            None
+                        },
+                        None,
+                    ]
+                }
+                "addx" => [1, 2].map(|i| {
+                    let ret = if cycles.contains(&cycle) {
+                        Some(x)
+                    } else {
+                        None
+                    };
+
+                    cycle += 1;
+                    if i == 2 {
+                        x += arg.trim().parse::<i32>().unwrap();
+                    }
+
+                    ret
+                }),
+                _ => panic!("invalid command :<"),
+            }
+        })
+        .flatten()
+        .zip(cycles)
+        .fold(0, |acc, (x, c)| acc + x * *c as i32)
 }
 
 fn main() {
-    println!("input: {}", day_fn(include_str!("input.txt")));
+    println!(
+        "sum of signal strength at given cycles: {}",
+        signal_strength(include_str!("input.txt"), &[20, 60, 100, 140, 180, 220])
+    );
 }
 
 #[cfg(test)]
@@ -12,6 +52,9 @@ mod test {
 
     #[test]
     fn test() {
-        assert_eq!(day_fn(include_str!("test.txt")), 0);
+        assert_eq!(
+            signal_strength(include_str!("test.txt"), &[20, 60, 100, 140, 180, 220]),
+            13140
+        );
     }
 }
