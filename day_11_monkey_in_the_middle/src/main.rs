ea7a34765ba63ed8a2f2a5867ea9af9ad9d7089f
Adenine <adenine.dev@gmail.com>
2022-12-13 00:25:13 -0500

    day 11 part 2

--- day_11_monkey_in_the_middle/src/main.rs
+++ day_11_monkey_in_the_middle/src/main.rs
@@ -2,13 +2,13 @@ use std::collections::VecDeque;
 
 #[derive(Debug)]
 enum Operation {
-    Add(u32),
-    Mul(u32),
+    Add(u64),
+    Mul(u64),
     Sqr,
 }
 
 impl Operation {
-    fn upon(&self, worry: u32) -> u32 {
+    fn upon(&self, worry: u64) -> u64 {
         match self {
             Self::Add(x) => worry + x,
             Self::Mul(x) => worry * x,
@@ -19,13 +19,13 @@ impl Operation {
 
 #[derive(Debug)]
 struct Test {
-    pub divisible_by: u32,
+    pub divisible_by: u64,
     pub if_true: usize,
     pub if_false: usize,
 }
 
 impl Test {
-    fn throws_to(&self, worry: u32) -> usize {
+    fn throws_to(&self, worry: u64) -> usize {
         if worry % self.divisible_by == 0 {
             self.if_true
         } else {
@@ -39,7 +39,7 @@ struct Monkey {
     pub idx: usize,
     pub operation: Operation,
     pub test: Test,
-    pub items: VecDeque<u32>,
+    pub items: VecDeque<u64>,
 }
 
 fn parse_monkeys(input: &str) -> Vec<Monkey> {
@@ -49,7 +49,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
             let mut lines = input.lines();
             let idx = {
                 let (_, idx) = lines.next().unwrap().split_once(' ').unwrap();
-                idx.split_once(':').unwrap().0.parse::<usize>().unwrap()
+                idx.split_once(':').unwrap().0.parse().unwrap()
             };
             let items = lines
                 .next()
@@ -58,15 +58,15 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                 .nth(1)
                 .unwrap()
                 .split(',')
-                .map(|s| s.trim().parse::<u32>().unwrap())
+                .map(|s| s.trim().parse().unwrap())
                 .collect();
             let operation = {
                 let (_, expr) = lines.next().unwrap().split_once("= ").unwrap();
                 let (lhs, rhs) = expr.split_once(['+', '*']).unwrap();
                 let lhs = lhs.trim();
                 let rhs = rhs.trim();
-                let lhs_n = lhs.parse::<u32>().unwrap_or(0);
-                let rhs_n = rhs.parse::<u32>().unwrap_or(0);
+                let lhs_n = lhs.parse().unwrap_or(0);
+                let rhs_n = rhs.parse().unwrap_or(0);
                 match if expr.find('*').is_some() { '*' } else { '+' } {
                     '*' => {
                         if lhs_n == 0 && rhs_n == 0 && lhs == "old" && rhs == "old" {
@@ -100,7 +100,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                     .split_once("Test: divisible by ")
                     .unwrap()
                     .1
-                    .parse::<u32>()
+                    .parse()
                     .unwrap(),
                 if_true: lines
                     .next()
@@ -108,7 +108,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                     .split_once("If true: throw to monkey ")
                     .unwrap()
                     .1
-                    .parse::<usize>()
+                    .parse()
                     .unwrap(),
                 if_false: lines
                     .next()
@@ -116,7 +116,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                     .split_once("If false: throw to monkey ")
                     .unwrap()
                     .1
-                    .parse::<usize>()
+                    .parse()
                     .unwrap(),
             };
             Monkey {
@@ -132,7 +132,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
     monkeys
 }
 
-fn monkeys_doing_monkey_things(input: &str) -> u32 {
+fn monkeys_doing_monkey_things(input: &str) -> u64 {
     let mut monkeys = parse_monkeys(input);
     let mut inspections = vec![0; monkeys.len()];
     for _ in 0..20 {
@@ -150,10 +150,34 @@ fn monkeys_doing_monkey_things(input: &str) -> u32 {
     inspections[inspections.len() - 1] * inspections[inspections.len() - 2]
 }
 
+fn monkeys_doing_worrying_monkey_things(input: &str) -> u64 {
+    let mut monkeys = parse_monkeys(input);
+    let mut inspections = vec![0; monkeys.len()];
+    let maximal_worry = monkeys.iter().map(|m| m.test.divisible_by).product::<u64>();
+    for _ in 0..10000 {
+        for i in 0..monkeys.len() {
+            while !monkeys[i].items.is_empty() {
+                let item = monkeys[i].items.pop_front().unwrap();
+                inspections[i] += 1;
+                let item = monkeys[i].operation.upon(item);
+                let target = monkeys[i].test.throws_to(item);
+                let item = item % maximal_worry;
+                monkeys[target].items.push_back(item);
+            }
+        }
+    }
+    inspections.sort();
+    inspections[inspections.len() - 1] * inspections[inspections.len() - 2]
+}
+
 fn main() {
     println!(
         "level of monkey business after 20 rounds of stuff-slinging simian shenanigans: {}",
         monkeys_doing_monkey_things(include_str!("input.txt"))
+    );
+    println!(
+        "level of worrying monkey business after 10000 rounds of stuff-slinging simian shenanigans: {}",
+        monkeys_doing_worrying_monkey_things(include_str!("input.txt"))
     );
 }
 
@@ -164,5 +188,10 @@ mod test {
     #[test]
     fn test() {
         assert_eq!(monkeys_doing_monkey_things(include_str!("test.txt")), 10605);
+
+        assert_eq!(
+            monkeys_doing_worrying_monkey_things(include_str!("test.txt")),
+            2713310158
+        );
     }
 }
