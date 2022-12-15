50154256000f6d2d09b16ceb714fa9e26c95d39e
Adenine <adenine.dev@gmail.com>
2022-12-15 02:36:34 -0500

    day 15 part 1

--- day_15_beacon_exclusion_zone/src/main.rs
+++ day_15_beacon_exclusion_zone/src/main.rs
@@ -1,9 +1,40 @@
-fn day_fn(input: &str) -> u32 {
-    0
+use std::collections::HashSet;
+
+fn excluded_positions(input: &str, row: i32) -> u32 {
+    let mut beacons = HashSet::new();
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    input.lines().for_each(|line| {
+        let [sx, sy, bx, by] = line
+                .split(['=', ',', ':'])
+                .flat_map(|x| x.parse::<i32>())
+                .collect::<Vec<_>>()[..] else { panic!("oof") };
+
+        if by == row {
+            beacons.insert(bx);
+        }
+
+        let dist = sx.abs_diff(bx) + sy.abs_diff(by);
+        let row_intersection_count = (2 * (dist + 1 - sy.abs_diff(row)).max(0)) as i32;
+
+        if row_intersection_count == 0 {
+            return;
+        }
+
+        let row_intersection_count = row_intersection_count - 1;
+        let rx = sx - (row_intersection_count) / 2;
+        min = min.min(rx);
+        max = max.max(rx + row_intersection_count);
+    });
+
+    ((max - min) - beacons.len() as i32) as u32
 }
 
 fn main() {
-    println!("input: {}", day_fn(include_str!("input.txt")));
+    println!(
+        "number of positions not panicking: {}",
+        excluded_positions(include_str!("input.txt"), 2000000)
+    );
 }
 
 #[cfg(test)]
@@ -12,6 +43,6 @@ mod test {
 
     #[test]
     fn test() {
-        assert_eq!(day_fn(include_str!("test.txt")), 0);
+        assert_eq!(excluded_positions(include_str!("test.txt"), 10), 26);
     }
 }
